8b06fd0d7ade498bf45fcbef4ddcaa8a3931d31e
alvin <givememyfiletofish@hotmail.com>
2020-11-11 17:36:12 -0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,11 @@ app.get("/crash-test", () => {
     throw new Error("Server will crash now");
   }, 0);
 });
-
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.post(
   "/signup",
   celebrate({
