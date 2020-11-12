14fdeaebe789446534119e4ef0c84bee94a1397e
alvin <givememyfiletofish@hotmail.com>
2020-11-12 16:56:01 -0500

    update files

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -71,7 +71,7 @@ const login = (req, res, next) => {
         httpOnly: true,
       });
 
-      res.send({token, text:"asdfasdf"});
+      res.send({text:"asdfasdf"});
     })
     .catch(next);
 };
