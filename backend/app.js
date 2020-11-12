0b0eb6672a12991aa48a4719fe60bc4bc917df06
alvin <givememyfiletofish@hotmail.com>
2020-11-12 00:10:15 -0500

    update files

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,7 @@ app.post(
   "/signup",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().email(),
+      email: Joi.string().required(),
       password: Joi.string().required(),
     }),
   }),
