e993f86443838ac43cea7c07c8e85aab946cb7d0
alvin <givememyfiletofish@hotmail.com>
2020-11-13 20:59:03 -0500

    update files

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -70,7 +70,6 @@ const login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       });
-
       res.send({ token, message: "successfully created token" });
     })
     .catch(next);
